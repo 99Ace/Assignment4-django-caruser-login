675d913a916e39125d351eb2aa42132111508db5
Ubuntu <ubuntu@ip-172-31-92-102.ec2.internal>
2019-09-03 04:42:00 +0000

    added logout function

--- assignment4/urls.py
+++ assignment4/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from accounts.views import index
+from accounts.views import index, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-]
+    path('logout/', logout, name='logout')]
