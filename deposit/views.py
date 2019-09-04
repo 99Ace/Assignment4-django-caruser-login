0bb094d207c4923e956771f0d6e1f968a7348b10
Ubuntu <ubuntu@ip-172-31-92-102.ec2.internal>
2019-09-04 14:04:42 +0000

    Added STRIPE Charge with Token rendered successfully

--- deposit/views.py
+++ deposit/views.py
@@ -6,12 +6,21 @@ from car_listing.models import Vehicle
 # Create your views here.
 def place_deposit(request,id):
     car = get_object_or_404(Vehicle, pk=id)
-    
-    amount = 200000 # prefix amount of $2,000 for deposit
-    key = settings.STRIPE_PUBLISHABLE_KEY
-    return render(request, 'place_deposit.html', {
-        'key' : key,
-        'amount' : amount,
-        'd' : car
+    deposit = 200000
+    if request.method == 'GET':
+        amount = deposit # prefix amount of $2,000 for deposit
+        key = settings.STRIPE_PUBLISHABLE_KEY
+        return render(request, 'place_deposit.html', {
+            'key' : key,
+            'amount' : amount,
+            'd' : car
         })
-    
+    else:
+        stripe.api_key = settings.STRIPE_SECRET_KEY #2
+        charge = stripe.Charge.create(
+            amount= deposit,
+            currency='sgd',
+            description='Deposit For Car',
+            source=request.POST['stripeToken']
+        )
+        return redirect(reverse('profile'))
